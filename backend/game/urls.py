3a9df443c9204f4ed46052c6a7fb2c1312c2182e
Ryan Brooks <Brooksryan63@gmail.com>
2026-03-28 19:30:08 -0400

    Updated game/urls.py and views.py to add a new endpoint that fetches today's puzzle from the DB and returns a JSON with some data about that puzzle.

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import health_check
+from .views import health_check, today_puzzle
 
 urlpatterns = [
     path("health/", health_check, name="health-check"),
+    path("puzzle/today/", today_puzzle, name="today-puzzle"),
 ]
--- backend/game/views.py
+++ backend/game/views.py
@@ -8,3 +8,23 @@ def health_check(request):
     """Return a JSON response confirming the backend is running."""
 
     return JsonResponse({"status": "ok"})
+
+
+def today_puzzle(request):
+    """Return brief JSON data for today's puzzle."""
+
+    today = date.today()
+    puzzle = Puzzle.objects.filter(date=today).first()
+    match puzzle:
+        case None:
+            return JsonResponse({
+                "exists": False,
+                "date": str(today)},
+                status=404)
+        case _:
+            return JsonResponse(
+                {
+                    "exists": True,
+                    "id": puzzle.id,
+                    "date": str(puzzle.date)}
+            )
